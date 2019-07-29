a3f21590171ed09eb290bdd1832e18283f16cc43
unknown <you@example.com>
2019-07-29 09:05:12 +0800

    修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <tab />
+    <Tab />
   </div>
 </template>
 
@@ -13,8 +13,8 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-    tab
+    HelloWorld
+    // tab
   }
 })
 export default class Home extends Vue {}
